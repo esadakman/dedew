f7bee7fa362e3314daec04e515a803018dfbbee2
esadakman <esadd26@gmail.com>
2022-09-17 21:20:27 +0300

    header

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react' 
+import "../styles/landing.css"
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <header className="header">
+        <div className="button-container">
+              <button className='header-button' >MINT CALENDAR</button>
+              <button className='header-button' >QUIZ</button> 
+        </div>        
+    </header>
   )
 }
 
 export default Landing
+
