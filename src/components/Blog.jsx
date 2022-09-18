fb11e2dda1f238ca78ccf4957a7a2fe6f617afa1
esadakman <esadd26@gmail.com>
2022-09-18 22:59:58 +0300

    ART section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import "../styles/blog.css";
 import Carousel from "react-bootstrap/Carousel";
 import background from "../assets/images/news_img.png";
+import johnDoel from "../assets/images/ceo_pp.png";
 
 const Blog = () => {
   return (
     <>
       {/* Carousel */}
-      <Carousel fade >
+      <Carousel fade>
         {/* // ! First Slide */}
-        <Carousel.Item > 
+        {/* <Carousel.Item > 
           <div className="text-center pt-5 text-light" ><h2>NEWS</h2></div>
           <div className="news-container d-flex flex-column justify-content-center align-items-center p-3">
             <div className="d-flex justify-content-center align-items-center flex-wrap p-3">
@@ -28,9 +29,9 @@ const Blog = () => {
               </div>
             </div>
           </div>
-        </Carousel.Item>
+        </Carousel.Item> */}
         {/* // ! Second Slide */}
-        <Carousel.Item>
+        {/* <Carousel.Item>
           <div className="text-center pt-5 text-light">
             <h2>BLOGS</h2>
           </div>
@@ -59,25 +60,10 @@ const Blog = () => {
               </div>
             </div>
           </div>
-        </Carousel.Item>
+        </Carousel.Item> */}
         {/* // ! Third Slide */}
 
-        <Carousel.Item>
-          <div className="text-center pt-5 text-light h2">
-            <h2>LEARN</h2>
-          </div>
-          <div className="carou-container d-flex flex-column align-items-center text-light py-3 px-5">
-            <div className="d-flex justify-content-center align-items-center flex-column"> 
-                <h1 h1 className="title-who m-5">WHO ARE WE ?</h1> 
-                <p className="desc w-75 my-5 text-justify px-5" >
-                  First, each and every member of this community is coming from various parts of the software sector. In this regard, in our community, our members' software experience is arithmetically more than fifteen years. So, the idea behind this team to come together is to give service to people in the area of software To do so, we are going to analyze our members and we will make some groups as a knowledge Overall, in this concept, our business connections will demand from our members to work with them
-                </p> 
-            </div>
-          </div>
-        </Carousel.Item>
-        {/* // ! Third Slide
-
-        <Carousel.Item>
+        {/* <Carousel.Item>
           <div className="text-center pt-5 text-light h2">
             <h2>LEARN</h2>
           </div>
@@ -90,6 +76,51 @@ const Blog = () => {
             </div>
           </div>
         </Carousel.Item> */}
+        {/* // ! Fourth Slide */}
+
+        {/* <Carousel.Item>
+          <div className="text-center pt-5 text-light h2">
+            <h2>ART</h2>
+          </div>
+          <div className="carou-container d-flex flex-column align-items-center text-light py-3 px-5">
+            <div className="d-flex justify-content-center align-items-center flex-column">
+              <div className="mt-3" >
+                <img className="pp" src={johnDoel} alt="" />
+              </div>
+              <p className="desc m-3 d-flex flex-column text-center">
+                John Doel
+                <span>CEO</span>
+              </p> 
+              <p className="desc w-50  text-center px-5 ">
+                Lorem ipsum dolor sit amet, consectetur ipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore
+              </p>
+            </div>
+          </div>
+        </Carousel.Item> */}
+        {/* // ! Fifth Slide */}
+
+        <Carousel.Item>
+          <div className="text-center pt-5 text-light h2">
+            <h2>CONTACT US</h2>
+          </div>
+          <div className="carou-container d-flex flex-column align-items-center text-light py-3 px-5">
+            <div className="social-icons  ">
+              <div className="links">
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-youtube"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-linkedin-in"></i>
+                <i class="fa-brands fa-discord"></i>
+                <i class="fa-solid fa-envelope"></i>
+              </div>
+              <p className="text-center mt-5">
+                Copyright &#169;
+                {new Date().getFullYear()}. All rights reserved.
+              </p>
+            </div>
+          </div>
+        </Carousel.Item>
       </Carousel>
       {/* 2.carousel */}
     </>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,10 +3,10 @@ import "../styles/news.css";
 
 const News = () => {
   return (
-    <div className="news-container d-flex flex-column  py-5  ">
-      <div className="text-center mt-5 text-light">
+    <div className="hi-container d-flex flex-column  py-4  ">
+      <div className="text-center  text-light">
         <h1 className="neon" >Hi</h1>
-        <h4>We are the creators of a future</h4>
+        <h6>We are the creators of a future</h6>
       </div>
     </div>
   );
