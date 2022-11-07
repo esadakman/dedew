66e549d57ca790eb75e7dacad99a8a01c069b1dc
esadakman <esadd26@gmail.com>
2022-11-07 12:52:37 +0300

    Blog & news frame-motion

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,93 +1,86 @@
 import React from "react";
 import blogStyle from "./blogStyle.module.scss";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 const Blog = () => {
-  const container = {
-    show: {
-      transition: {
-        staggerChildren: 0.35,
-      },
-    },
-  };
-  const item = {
-    hidden: { opacity: 0, y: 200 },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        ease: [0.6, 0.01, -0.05, 0.95],
-        duration: 1.6,
-      },
-    },
-    exit: {
-      opacity: 0,
-      y: -200,
-      transition: {
-        ease: "easeInOut",
-        duration: 0.8,
-      },
-    },
-  };
-  const itemMain = {
-    hidden: { opacity: 0, y: 200 },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        ease: [0.6, 0.01, -0.05, 0.95],
-        duration: 1.6,
-      },
-    },
-  }; 
-  return (
-    <>
-      <section className={blogStyle["container"]}>
-        <header className="text-center pt-5 text-light">
-          <h2>BLOG</h2>
-        </header>
-        <motion.div className={blogStyle["blog-container"]}>
-          <motion.div
-            className={blogStyle["content-container"]}
-            variants={container} 
-            initial="hidden"
-            animate="show"
-            exit="exit"
-          >
-            <motion.div variants={item} className={blogStyle["blog-left"]}>
-              <div
-                variants={item}
-                className={blogStyle["circle_1"]} 
-              ></div>
-              <div
-                variants={item}
-                className={blogStyle["circle_2"]}
-              ></div>
-              <div
-                variants={item}
-                className={blogStyle["circle_3"]}
-              ></div>
-            </motion.div>
+  const { ref, inView } = useInView();
+  const animationLeft = useAnimation();
+  const animationRight = useAnimation();
 
-            <motion.div variants={itemMain} className={blogStyle["blog-right"]}>
-              <div variants={item} className={blogStyle["circle-up"]}>
-                <div className={blogStyle["circle-right"]}></div>
-              </div>
-              <div variants={item} className={blogStyle["content"]}>
-                <p>
-                  Examination will start 14th June 20 March 2022 *discord and
-                  slack channels members will enter exam for improving positions
-                  03.20.22 each three months *our team announce to you we are
-                  going to create one more nft collection for designers... 16
-                  March 2022 *we will meet in north macedonia for having fun and
-                  introducing *our partnership coltra will describe us !! ...
-                </p>
-              </div>
-            </motion.div>
+  useEffect(() => {
+    if (inView) {
+      animationLeft.start({
+        opacity: 1,
+        y: 0,
+        transition: {
+          type: "easeInOut",
+          duration: 1,
+        },
+      });
+      animationRight.start({
+        opacity: 1,
+        y: 0,
+        transition: {
+          type: "easeInOut",
+          duration: 1,
+        },
+      });
+    }
+    if (!inView) {
+      animationLeft.start({
+        opacity: 0,
+        y: "-100vh",
+      });
+      animationRight.start({
+        opacity: 0,
+        y: "100vh",
+      });
+    }
+    // console.log("use effecthook, inView =", inView);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
+
+  return (
+    <section className={blogStyle["container"]} ref={ref}>
+      <header className="text-center pt-5 text-light">
+        <h2>BLOG</h2>
+      </header>
+      <div className={blogStyle["blog-container"]}>
+        <motion.div className={blogStyle["content-container"]}>
+          <motion.div
+            className={blogStyle["blog-left"]}
+            animate={animationLeft}
+            // initial={{ opacity: 0, y: -100 }}
+            // animate={{ opacity: 1, y: 0 }}
+            // transition={{ type: "easeInOut", duration: 1 }}
+          >
+            <div className={blogStyle["circle_1"]}></div>
+            <div className={blogStyle["circle_2"]}></div>
+            <div className={blogStyle["circle_3"]}></div>
+          </motion.div>
+          <motion.div
+            animate={animationRight}
+            className={blogStyle["blog-right"]}
+          >
+            <div className={blogStyle["circle-up"]}>
+              <div className={blogStyle["circle-right"]}></div>
+            </div>
+            <div className={blogStyle["content"]}>
+              <p>
+                Examination will start 14th June 20 March 2022 *discord and
+                slack channels members will enter exam for improving positions
+                03.20.22 each three months *our team announce to you we are
+                going to create one more nft collection for designers... 16
+                March 2022 *we will meet in north macedonia for having fun and
+                introducing *our partnership coltra will describe us !! ...
+              </p>
+            </div>
           </motion.div>
         </motion.div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,63 +1,60 @@
 import background from "../../assets/images/carou_img_1.png";
 import newsStyle from "./newsStyle.module.scss";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 function News() {
-  const container = {
-    show: {
-      transition: {
-        staggerChildren: 0.35,
-      },
-    },
-  };
-  const item = {
-    hidden: { opacity: 0, y: 200 },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        ease: [0.6, 0.01, -0.05, 0.95],
-        duration: 1.6,
-      },
-    },
-    exit: {
-      opacity: 0,
-      y: -200,
-      transition: {
-        ease: "easeInOut",
-        duration: 0.8,
-      },
-    },
-  };
-  const itemMain = {
-    hidden: { opacity: 0, y: 200 },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        ease: [0.6, 0.01, -0.05, 0.95],
-        duration: 1.6,
-      },
-    },
-  };
+  const { ref, inView } = useInView();
+  const animationLeft = useAnimation();
+  const animationRight = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animationLeft.start({
+        x: 0,
+        transition: {
+          type: "spring",
+          duration: 1,
+          bounce: 0.3,
+        },
+      });
+      animationRight.start({
+        x: 0,
+        transition: {
+          type: "spring",
+          duration: 1,
+          bounce: 0.2,
+        },
+      });
+    }
+    if (!inView) {
+      animationLeft.start({
+        x: "-500vw",
+      });
+      animationRight.start({
+        x: "500vw",
+      });
+    }
+    // console.log("use effecthook, inView =", inView);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
+
   return (
-    <section id="news">
+    <section ref={ref} id="news">
       <div className={newsStyle["container"]}>
         <div className="text-center pt-5 text-light">
           <h2>NEWS</h2>
         </div>
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          exit="exit"
-          className={newsStyle["content"]}
-        >
-          <motion.div variants={item} className={newsStyle["imageDiv"]}>
-            <img variants={item} src={background} alt="background png" />
+        <motion.div className={newsStyle["content"]}>
+          <motion.div className={newsStyle["imageDiv"]} animate={animationLeft}>
+            <img src={background} alt="background png" />
           </motion.div>
-          <motion.div variants={itemMain} className={newsStyle["content-text"]}>
-            <p variants={item}>
+          <motion.div
+            className={newsStyle["content-text"]}
+            animate={animationRight}
+          >
+            <p>
               Examination will start 14th June 20 March 2022 *discord and slack
               channels members will enter exam for improving positions 03.20.22
               each three months *our team announce to you we are going to create
